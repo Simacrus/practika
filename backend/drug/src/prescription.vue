68f9e3f103d23b9d13e25a607ba0a214e241d93d
Simacrus <99753312+Simacrus@users.noreply.github.com>
2023-05-31 19:20:35 +0300

    Update prescription.vue

--- backend/drug/src/prescription.vue
+++ backend/drug/src/prescription.vue
@@ -1,11 +1,8 @@
 <script>
     export default {
-        name: 'drugs',
-        components: {
-            prescriptions
-        };
         data() {
             return {
+                token_user: `${localStorage.getItem('token')}`,
                 id_prescription: '',
                 drug_name: '',
                 days: '',
@@ -14,26 +11,135 @@
             }
         },
 
+        created: function () {
+            this.GetDrugss();
+
+            //настройка setTimeout
+            //внутри - перебор drugs
+        },
+        methods: {
+            getRest(drug) {
+                console.log(drug)
+
+            },
+            Logout() {
+                const token = localStorage.getItem('token');
+                const res = fetch(`http://localhost:5000/api/logout/?token_user=${token}`, {
+                    method: 'Get',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
 
 
-            methods: {
-                GetDrugs() {
-                    const res = fetch('http://localhost:5000/api/drugs/prescriptions', {
-                        method: 'Get',
+                })
+                localStorage.removeItem("token");
+                //localStorage.setItem("token", null);
+                res.then(res => res.text()).then(res => alert(res))
+            }
+            ,
+
+            GetDrugs() {
+                let self = this;
+                const token = localStorage.getItem('token');
+                const res = fetch(`http://localhost:5000/api/drugs/prescriptions` + new URLSearchParams({
+                    token_user: token
+                }), {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+
+                })
+
+
+                res.then(res => res.json()).then(res => { console.log(res); self.drugs = res })
+
+            },
+
+
+            CreateDrug() {
+                let self = this;
+                const token = localStorage.getItem('token');
+                const res = fetch(`http://localhost:5000/api/CreateTimer`, {
+                    method: 'Post',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+
+                    body: JSON.stringify({
+                        id_timer: this.id_timer,
+                        timer_name: this.timer_name,
+                        timer_runnig_time: this.timer_runnig_time,
+                        token_user: token,
+                    })
+
+                })
+
+
+                res.then(res => res.json()).then(res => self.drugs.push(res))
+
+
+            },
+            //// метод остановки таймер
+
+            Pause(id_timer) {
+                const token = localStorage.getItem('token');
+                const res = fetch(`http://localhost:5000/api/Pause/${id_timer}`, {
+                    method: 'Post',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+
+                    body: JSON.stringify({
+                        token_user: token,
+                    })
+
+                })
+                localStorage.removeItem("token");
+
+                res.then(res => res.text()).then(res => alert(res))
+            },
+
+            // Метод старта таймера
+
+            Resume(id_timer) {
+                const token = localStorage.getItem('token');
+                const res = fetch(`http://localhost:5000/api/Resume/${id_timer}`, {
+                    method: 'Post',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+
+                    body: JSON.stringify({
+                        token_user: token,
+                    })
+
+                })
+                localStorage.removeItem("token");
+
+                res.then(res => res.text()).then(res => alert(res))
+            },
+            //список таймера
+            ListTimer: {
+                List() {
+                    const token = localStorage.getItem('token');
+                    const res = fetch(`http://localhost:5000/api/CreateTimer/?token_user=${token}`, {
+                        method: 'Post',
                         headers: {
                             'Content-Type': 'application/json'
                         },
 
+                        body: JSON.stringify({
+                            timer_name: this.timer_name,
+                            timer_runnig_time: this.timer_runnig_time
                         })
-                    })
-                    res.then(res => res.text()).then(res=>console.log(res)).catch()
-                    
 
+                    })
+                    localStorage.removeItem("token");
+
+                    res.then(res => res.text()).then(res => alert(res))
                 }
-            }
-        }
-        mounted() {
-            this.GetDrugs();
+            },
         }
     }
 </script>
